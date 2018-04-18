43e15d29856eed5fa984738fcc9eb0459dcb2e1a
mauricio_mmarques <mauricio.mmarques1@gmail.com>
2018-04-17 22:45:18 -0300

    Add face detection functions

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 from importlib import import_module
 import os
 from flask import Flask, render_template, Response
+import cv2
+import imutils
 
 # import camera driver
 #if os.environ.get('CAMERA'):
@@ -13,7 +15,9 @@ from flask import Flask, render_template, Response
 from camera_pi import Camera
 
 app = Flask(__name__)
-
+hog = cv2.HOGDescriptor()
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+counter = 0
 
 @app.route('/')
 def index():
@@ -25,8 +29,76 @@ def gen(camera):
     """Video streaming generator function."""
     while True:
         frame = camera.get_frame()
+        if counter == 10:
+            print("Counter 10")
+            detect_people(frame)
+            detect_faces(frame)
+            
+            counter = 0
+        else:
+            counter += 1
+            
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
+def detect_people(frame):
+    image = imutils.resize(frame, width=min(400, image.shape[1]))
+
+    # detect people in the image
+    (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+            padding=(8, 8), scale=1.05)
+    
+    # draw the final bounding boxes
+    for (xA, yA, xB, yB) in pick:
+        print("Person xA:{}, xB:{}, yA{}, yB".format(xA, xB, yA, yB))
+        
+def detect_faces(frame):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.2,
+                minNeighbors=10,
+                minSize=(16, 16)
+            )
+    for face in faces:
+        face_img, cropped = crop_face(image, face, margin=40)
+        (x, y, w, h) = cropped
+        print("Face xA:{}, xB:{}, yA{}, yB".format(x, w, y, h))
+        
+def crop_face(imgarray, section, margin=40, size=16):
+        """
+        :param imgarray: full image
+        :param section: face detected area (x, y, w, h)
+        :param margin: add some margin to the face detected area to include a full head
+        :param size: the result image resolution with be (size x size)
+        :return: resized image in numpy array with shape (size x size x 3)
+        """
+
+        img_h, img_w, _ = imgarray.shape
+        if section is None:
+            section = [0, 0, img_w, img_h]
+        (x, y, w, h) = section
+        margin = int(min(w,h) * margin / 100)
+        x_a = x - margin
+        y_a = y - margin
+        x_b = x + w + margin
+        y_b = y + h + margin
+        if x_a < 0:
+            x_b = min(x_b - x_a, img_w-1)
+            x_a = 0
+        if y_a < 0:
+            y_b = min(y_b - y_a, img_h-1)
+            y_a = 0
+        if x_b > img_w:
+            x_a = max(x_a - (x_b - img_w), 0)
+            x_b = img_w
+        if y_b > img_h:
+            y_a = max(y_a - (y_b - img_h), 0)
+            y_b = img_h
+        cropped = imgarray[y_a: y_b, x_a: x_b]
+        resized_img = cv2.resize(cropped, (size, size), interpolation=cv2.INTER_AREA)
+        resized_img = np.array(resized_img)
+        return resized_img, (x_a, y_a, x_b - x_a, y_b - y_a)
 
 
 @app.route('/video_feed')
@@ -37,4 +109,5 @@ def video_feed():
 
 
 if __name__ == '__main__':
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
     app.run(host='0.0.0.0', threaded=True)
